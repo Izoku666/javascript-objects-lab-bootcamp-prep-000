d29611df85dda29c7a6a2a4523b02077e0610db0
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 03:03:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function destructivelyUpdateObjectWithKeyAndValue(obj , key , value)
 function deleteFromObjectByKey(obj , key)
 {
   var newObj = Object.assign({} , obj);
-  delete obj[key];
+  delete newObj[key];
   return newObj;
 }
 

37c3ba6a09df4d7b611eb2a7cb8a1921ccaf379a
Martin Spicuzza <spicuzza157@gmail.com>
2019-03-13 02:52:49 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,22 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(obj , key , value)
+{
+  
+  //return new recipes({ [key] : [value]});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(obj , key , value)
+{
+  
+}
+
+function deleteFromObjectByKey(obj , key)
+{
+  
+}
+
+function destructivelyDeleteFromObjectByKey(obj , key)
+{
+  
+}
